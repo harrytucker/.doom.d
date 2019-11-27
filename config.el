9b62c63d5e47f9c82dba3e14c9007dfeb79d4006
Harry Tucker <harrytuckerubuntu@gmail.com>
2019-11-27 22:44:02 +0000

    Change font and configure neotree resizing

--- config.el
+++ config.el
@@ -2,5 +2,9 @@
 
 ;; Place your private configuration here
 
-;; Set emacs theme
+;; UI Configuration
 (setq doom-theme 'doom-dracula)
+(setq doom-font (font-spec :family "Fira Code" :size 14))
+
+;; Window Configuration
+(setq neo-window-fixed-size nil)
