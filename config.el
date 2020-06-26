d44cec6cbfd8507dd0e8950184573724465c4a71
Harry Tucker <harry.tucker@hpe.com>
2020-06-26 15:14:59 +0100

    Configure Org->LaTeX export to preferences

--- config.el
+++ config.el
@@ -1,5 +1,18 @@
 (setq user-full-name "Harry Tucker")
 
+(require 'ox-latex)
+
+(add-to-list 'org-latex-packages-alist '("" "minted"))
+(add-to-list 'org-latex-packages-alist '("" "color"))
+
+(setq org-latex-listings 'minted
+      org-latex-pdf-process
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+(add-to-list 'org-latex-packages-alist '("" "booktabs"))
+
 (setq doom-theme 'doom-dracula)
 (setq doom-font (font-spec :family "Fira Code" :size 14))
 
