2a93b5ef1b28ef8a13dc68c02f20327310360d8c
Harry Tucker <harrytuckerubuntu@gmail.com>
2023-03-21 22:43:22 +0000

    Switch everything over to use-package!
    This updated config will should now make proper usage of deferred
    loading for improved init times. I've also gone through and more
    thoroughly documented all the lisp code, and consolidated any disparate
    config so it's all held together more nicely.
    
    Now if I need to share my config with anyone, it should be much easier
    for me to be sure I've shown them everything I have, and the lisp should
    be much easier for them to grok.

--- config.el
+++ config.el
@@ -1,44 +1,108 @@
-(setq user-full-name "Harry Tucker" ; personal info
+;; Emacs User Identification
+(setq user-full-name "Harry Tucker"
       user-mail-address "tucker.harry@outlook.com")
 
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode) ; rainbow brackets!
-
-(setq doom-theme 'doom-dracula                  ; set doom theme
-      doom-themes-treemacs-theme "doom-colors"  ; set treemacs theme
-      doom-font (font-spec                      ; set font family
+;; Set themes for Doom and Treemacs, and select the JetBrains Mono font
+(setq doom-theme 'doom-dracula
+      doom-themes-treemacs-theme "doom-colors"
+      doom-font (font-spec
                  :family "JetBrains Mono"
+                 :weight 'bold
                  :size 14))
 
+;; Constructs splash image path by concatenating the configured Doom config
+;; directory with the image path.
+;;
+;; This assumes you've pulled this from Git to get the images for my config.
 (setq fancy-splash-image (concat doom-user-dir "images/power-splash.png"))
 
-(setq doom-modeline-major-mode-icon t        ; enable modeline major-mode icon
-      doom-modeline-major-mode-color-icon t) ; use coloured icons
+;; Enables modeline icons with colour support
+(setq doom-modeline-major-mode-icon t
+      doom-modeline-major-mode-color-icon t)
 
-(evil-set-undo-system 'undo-tree) ; tree-based undo and redo functionality
+;; Highlights delimiter pairs with varying colours
+(add-hook! 'prog-mode-hook #'rainbow-delimiters-mode)
 
-;; vcxsrv on windows has issues with this, so only enable on MacOS
-(if IS-MAC (add-to-list 'default-frame-alist '(fullscreen . fullboth)))
+;; Allows undoing and redoing as a tree of changes, instead of being limited to
+;; linear changes
+(evil-set-undo-system 'undo-tree)
 
+;; If running on a Mac, automatically fullscreen on launch
+(if IS-MAC (add-to-list 'default-frame-alist'(fullscreen . fullboth)))
+
+;; Provides configuration for working with 'eshell', a shell within Emacs
+(use-package! eshell
+  :defer
+  :config
+  ;; Sets the $TERM environment variable to indicate colour support
+  (setq eshell-term-name "xterm-256-color"))
+
+;; Provides configuration for working with SQL within Emacs
 (use-package! sql
   :defer
   :config
   (sql-set-product 'postgres))
 
+;; Provides configuration for using 'eslint' with Emacs
 (use-package! lsp-eslint
   :defer
   :config
+  ;; Automatically fix trivial lint failures
   (setq lsp-eslint-auto-fix-on-save t))
 
+;; Provides configuration for working with the Rust programming language
 (use-package! rustic
   :defer
   :config
+  ;; Use 'rust-analyser', and enable options that allow further completion
+  ;; support for crates that rely on macros and build scripts
   (setq lsp-rust-server 'rust-analyzer
         lsp-rust-analyzer-proc-macro-enable t
         lsp-rust-analyzer-cargo-run-build-scripts t))
 
+;; Provides better Emacs support for Python docstrings
+(use-package! python-docstring-mode
+  :hook python-mode)
+
+;; Provides configuration for using the Debug Adapter Protocol from Emacs
 (use-package! dap-mode
   :defer
+  :init
+  ;; Provides key map for interacting with 'dap-mode' for debugging supported
+  ;; languages
+  (map! :map dap-mode-map
+        :leader
+        :prefix ("d" . "dap")
+        ;; basics
+        :desc "dap next"          "n" #'dap-next
+        :desc "dap step in"       "i" #'dap-step-in
+        :desc "dap step out"      "o" #'dap-step-out
+        :desc "dap continue"      "c" #'dap-continue
+        :desc "dap hydra"         "h" #'dap-hydra
+        :desc "dap debug restart" "r" #'dap-debug-restart
+        :desc "dap debug"         "s" #'dap-debug
+
+        ;; debug
+        :prefix ("dd" . "Debug")
+        :desc "dap debug recent"  "r" #'dap-debug-recent
+        :desc "dap debug last"    "l" #'dap-debug-last
+
+        ;; eval
+        :prefix ("de" . "Eval")
+        :desc "eval"                "e" #'dap-eval
+        :desc "eval region"         "r" #'dap-eval-region
+        :desc "eval thing at point" "s" #'dap-eval-thing-at-point
+        :desc "add expression"      "a" #'dap-ui-expressions-add
+        :desc "remove expression"   "d" #'dap-ui-expressions-remove
+
+        ;; breakpoints
+        :prefix ("db" . "Breakpoint")
+        :desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
+        :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
+        :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
+        :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message)
   :config
+  ;; Rust template for DAP debugging
   (dap-register-debug-template "Rust::GDB Run Configuration"
                                (list :type "gdb"
                                      :request "launch"
@@ -47,46 +111,17 @@
                                      :target nil
                                      :cwd nil)))
 
-(use-package! python-docstring-mode
-  :hook python-mode)
-
-(map! :map dap-mode-map
-      :leader
-      :prefix ("d" . "dap")
-      ;; basics
-      :desc "dap next"          "n" #'dap-next
-      :desc "dap step in"       "i" #'dap-step-in
-      :desc "dap step out"      "o" #'dap-step-out
-      :desc "dap continue"      "c" #'dap-continue
-      :desc "dap hydra"         "h" #'dap-hydra
-      :desc "dap debug restart" "r" #'dap-debug-restart
-      :desc "dap debug"         "s" #'dap-debug
-
-      ;; debug
-      :prefix ("dd" . "Debug")
-      :desc "dap debug recent"  "r" #'dap-debug-recent
-      :desc "dap debug last"    "l" #'dap-debug-last
-
-      ;; eval
-      :prefix ("de" . "Eval")
-      :desc "eval"                "e" #'dap-eval
-      :desc "eval region"         "r" #'dap-eval-region
-      :desc "eval thing at point" "s" #'dap-eval-thing-at-point
-      :desc "add expression"      "a" #'dap-ui-expressions-add
-      :desc "remove expression"   "d" #'dap-ui-expressions-remove
-
-      :prefix ("db" . "Breakpoint")
-      :desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
-      :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
-      :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
-      :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message)
-
-(require 'ox-latex)   ; required for config
-(require 'ox-bibtex)
-
+;; Provides configuration for working with 'org-mode'
 (use-package! org
   :defer
   :config
+  ;; Hide emphasis markers that wrap text (i.e. bold, italics)
+  (setq org-hide-emphasis-markers t)
+  ;; Wrap text at 80 characters for better Git diffs and readability
+  (add-hook! 'org-mode-hook #'auto-fill-mode)
+  ;; Use 'pdf-tools' as the default viewer for exported Org documents
+  (add-to-list 'org-file-apps '("\\.pdf\\'" . pdf-tools))
+  ;; Enlarge top and second level heading fonts
   (custom-set-faces!
     '(org-level-1
       :height 1.2
@@ -95,17 +130,24 @@
       :height 1.1
       :inherit outline-2)))
 
+;; Provides 'org-modern' configuration in place of Doom's (org +pretty)
 (use-package! org-modern
   :after org
   :config
+  ;; Disable table formatting and star hiding, increase label border
   (setq org-modern-table nil
         org-modern-hide-stars nil
         org-modern-label-border 0.3)
+  ;; Enable org-modern globally
   (global-org-modern-mode))
 
+;; Provides configuration for generating LaTeX using 'org-mode'
 (use-package! org
   :defer
   :config
+  ;; Enable export support for LaTeX and BibTeX formats
+  (require 'ox-latex)
+  (require 'ox-bibtex)
   ;; Better syntax highlighting in exported LaTeX
   (setq org-latex-src-block-backend 'minted)
   ;; Enable additional packages for exported LaTeX, takes the form:
@@ -114,21 +156,7 @@
                                    ("" "tabularx")
                                    ("" "color")
                                    ("newfloat" "minted")))
-  ;; Wrap text at 80 characters for better Git diffs and readability
-  (add-hook! 'org-mode-hook #'auto-fill-mode))
-
-(use-package! org-tree-slide
-  :after org
-  :config
-  ;; Hide formatting characters, use top-level headings as slides
-  (setq org-hide-emphasis-markers t
-        org-tree-slide-skip-outline-level 2)
-  ;; Use the fancy presentation profile, shiny animations!
-  (org-tree-slide-presentation-profile))
-
-(use-package! org
-  :defer
-  :config
+  ;; Define 'mimore' LaTeX document class for use in exports
   (add-to-list 'org-latex-classes
                '("mimore"
                  "\\documentclass{mimore}\n[NO-DEFAULT-PACKAGES\]\n[PACKAGES\]\n[EXTRA\]"
@@ -138,11 +166,16 @@
                  ("\\paragraph{%s}" . "\\paragraph\*{%s}")
                  ("\\subparagraph{%s}" . "\\subparagraph\*{%s}"))))
 
-(use-package! org
-  :defer
+;; Provides support for presenting directly from 'org-mode' buffers
+(use-package! org-tree-slide
+  :after org
   :config
-  (add-to-list 'org-file-apps '("\\.pdf\\'" . pdf-tools)))
+  ;; Hide formatting characters, use top-level headings as slides
+  (setq org-tree-slide-skip-outline-level 2)
+  ;; Use the fancy presentation profile, shiny animations!
+  (org-tree-slide-presentation-profile))
 
+;; Provides configuration for 'org-roam', an Emacs knowledge graph
 (use-package! org-roam
   :defer
   :config
@@ -152,9 +185,11 @@
           "http"
           "https")))
 
+;; Provides 'websocket', a dependency of 'org-roam-ui'
 (use-package! websocket
   :after org-roam)
 
+;; Provides 'org-roam-ui' a web frontend for 'org-roam'
 (use-package! org-roam-ui
   :after org-roam
   :config
@@ -165,12 +200,14 @@
         org-roam-ui-update-on-save t
         org-roam-ui-open-on-start t))
 
+;; Provides configuration for interacting with Kubernetes clusters from within
+;; Emacs
 (use-package! kele
+  :init
+  (map! :leader
+        :desc "Kubernetes" "k" #'kele-dispatch)
   :config
   (kele-mode 1))
-
-(map! :leader
-      :desc "Kubernetes" "k" #'kele-dispatch)
 
 (setq pdf-view-use-scaling t          ; MacOS specific workarounds
       pdf-view-use-imagemagick nil)
